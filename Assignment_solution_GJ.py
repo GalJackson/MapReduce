f9b72033ae34e0730ef8cd311c3bee53cc02022c
Gal <zr016214@student.reading.ac.uk>
2024-03-26 19:50:28 +0000

    moving the hardcoded file paths to a variable + cleaning

--- Assignment_solution_GJ.py
+++ Assignment_solution_GJ.py
@@ -4,6 +4,8 @@
 import multiprocessing as mp
 import csv
 
+input_file_path = r'./AComp_Passenger_data_no_error.csv'
+
 def map(r):
     try:
         return (r[0], 1)
@@ -29,13 +31,17 @@ def reduce(kv):
 
 if __name__ == '__main__':
     map_in = []
+
     # open the CSV file
-    with open(r'./AComp_Passenger_data_no_error.csv', 'r', encoding='utf-8') as file:
+    with open(input_file_path, 'r', encoding='utf-8') as file:
         map_in = list(csv.reader(file, delimiter=','))
+
         with mp.Pool(processes=mp.cpu_count()) as pool:
             map_out = pool.map(map, map_in)
             print(map_out)
             reduce_in = shuffle(map_out)
             print(reduce_in)
             reduce_out = pool.map(reduce, reduce_in.items())
-    print(reduce_out)
+    
+    frequent_flyer = reduce_out[0]
+    print(f"The passenger with the most flights is passenger {frequent_flyer[0]} with a total of {frequent_flyer[1]} flights.")
